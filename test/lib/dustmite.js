35557bf80d0bbc9c511c4cfe70b08898d9bb93d8
Andrew Walker <andy@moddular.org>
2015-10-15 14:08:37 +0100

    Allow custom Reporters

--- test/lib/dustmite.js
+++ test/lib/dustmite.js
@@ -4,26 +4,30 @@
 var assert = require('proclaim');
 var path = require('path');
 
-var Dustmite = require(path.join(__dirname, '..', '..', 'lib', 'dustmite'));
+var dustmite = require(path.join(__dirname, '..', '..', 'lib', 'dustmite'));
 
 describe('lib/dustmite', function() {
-	it('Should export a `Validator` function', function() {
-		assert.isFunction(Dustmite.Validator);
+	it('Should export a function', function() {
+		assert.isFunction(dustmite);
+	});
+
+	it('Should return a function when dustmite is called', function() {
+		assert.isFunction(dustmite());
 	});
 
 	it('Should export a `Reporter` function', function() {
-		assert.isFunction(Dustmite.Reporter);
+		assert.isFunction(dustmite.Reporter);
 	});
 
 	it('Should export a `Parser` function', function() {
-		assert.isFunction(Dustmite.Parser);
+		assert.isFunction(dustmite.Parser);
 	});
 
 	it('Should export a `Node` function', function() {
-		assert.isFunction(Dustmite.Node);
+		assert.isFunction(dustmite.Node);
 	});
 
 	it('Should export a `Rules` object', function() {
-		assert.isObject(Dustmite.Rules);
+		assert.isObject(dustmite.Rules);
 	});
 });
